734681a4890a49790ff284c754eb376ed7ce31cc
Gabriel <gabriel.gabrielyea@gmail.com>
2021-12-12 12:26:08 -0600

    Add params

--- spec/requests/api/v1/planes_spec.rb
+++ spec/requests/api/v1/planes_spec.rb
@@ -19,6 +19,9 @@ RSpec.describe 'api/v1/planes', type: :request do
     end
 
     post('create plane') do
+      parameter name: 'model', in: :path, type: :string, description: 'Plane model'
+      parameter name: 'description', in: :path, type: :string, description: 'Plane description'
+      parameter name: 'tour_price', in: :path, type: :string, description: 'Tour price'
       response(200, 'successful') do
 
         after do |example|
@@ -36,49 +39,10 @@ RSpec.describe 'api/v1/planes', type: :request do
   path '/api/v1/planes/{id}' do
     # You'll want to customize the parameter types...
     parameter name: 'id', in: :path, type: :string, description: 'id'
-    parameter name: 'model', in: :path, type: :string, description: 'Plane model'
-    parameter name: 'description', in: :path, type: :string, description: 'Plane description'
-    parameter name: 'tour_price', in: :path, type: :string, description: 'Tour cost'
 
     get('show plane') do
       response(200, 'successful') do
         let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-      response(200, 'successful') do
-        let(:model) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-      response(200, 'successful') do
-        let(:description) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-      response(200, 'successful') do
-        let(:tour_price) { '123' }
 
         after do |example|
           example.metadata[:response][:content] = {
