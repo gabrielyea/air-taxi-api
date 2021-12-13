a2e7108e7684fc8b8b1abf925666b14e630bb43d
Gabriel <gabriel.gabrielyea@gmail.com>
2021-12-13 09:37:16 -0600

    Update endpoints

--- spec/requests/api/v1/registrations_spec.rb
+++ spec/requests/api/v1/registrations_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'api/v1/registrations', type: :request do
   path '/api/signup/cancel' do
 
     get('cancel registration') do
+      tags 'Registration'
       response(200, 'successful') do
 
         after do |example|
@@ -22,6 +23,7 @@ RSpec.describe 'api/v1/registrations', type: :request do
   path '/api/signup/sign_up' do
 
     get('new registration') do
+      tags 'Registration'
       response(200, 'successful') do
 
         after do |example|
@@ -39,6 +41,7 @@ RSpec.describe 'api/v1/registrations', type: :request do
   path '/api/signup/edit' do
 
     get('edit registration') do
+      tags 'Registration'
       response(200, 'successful') do
 
         after do |example|
@@ -56,6 +59,7 @@ RSpec.describe 'api/v1/registrations', type: :request do
   path '/api/signup' do
 
     patch('update registration') do
+      tags 'Registration'
       response(200, 'successful') do
 
         after do |example|
@@ -70,6 +74,7 @@ RSpec.describe 'api/v1/registrations', type: :request do
     end
 
     put('update registration') do
+      tags 'Registration'
       response(200, 'successful') do
 
         after do |example|
@@ -84,6 +89,7 @@ RSpec.describe 'api/v1/registrations', type: :request do
     end
 
     delete('delete registration') do
+      tags 'Registration'
       response(200, 'successful') do
 
         after do |example|
@@ -98,15 +104,21 @@ RSpec.describe 'api/v1/registrations', type: :request do
     end
 
     post('create registration') do
-      response(200, 'successful') do
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
+      tags 'Registration'
+      consumes 'application/json'
+      parameter name: :signup, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          email: { type: :string },
+          password: { type: :string },
+          password_confirmation: { type: :string },
+        },
+        required: [ 'email', 'password', 'password_confirmation', 'name' ]
+      }
+      
+      response('201', 'You are login!') do
+        let(:signup) { { name: 'testname', email: '123456@gmail.com', password: '123456', password_confirmation: '123456' } }
         run_test!
       end
     end
--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'api/v1/sessions', type: :request do
   path '/api/login' do
 
     get('new session') do
+      tags 'Session'
       response(200, 'successful') do
 
         after do |example|
@@ -19,15 +20,19 @@ RSpec.describe 'api/v1/sessions', type: :request do
     end
 
     post('create session') do
-      response(200, 'successful') do
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
+      tags 'Session'
+      consumes 'application/json'
+      parameter name: :login, in: :body, schema: {
+        type: :object,
+        properties: {
+          email: { type: :string },
+          password: { type: :string },
+        },
+        required: [ 'email', 'password' ]
+      }
+      
+      response('201', 'You are login!') do
+        let(:login) { { email: '123456@gmail.com', password: '123456' } }
         run_test!
       end
     end
@@ -36,6 +41,7 @@ RSpec.describe 'api/v1/sessions', type: :request do
   path '/api/logout' do
 
     delete('delete session') do
+      tags 'Session'
       response(200, 'successful') do
 
         after do |example|
